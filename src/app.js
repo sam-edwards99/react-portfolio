bc0ba3c5d4dc8056cef0a7c0ac60f5eb92b268d0
Sam Edwards <nevitt1@umbc.edu>
2020-10-02 16:40:49 -0400

    Initial commit, part way thru part 8 of tutorial

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+    render(){
+        return (
+            <div>
+                <h1>Hello There</h1>
+                <p>I am Sam</p>
+            </div>
+        )
+    }
+}
+
+
+
+
+export default App;
