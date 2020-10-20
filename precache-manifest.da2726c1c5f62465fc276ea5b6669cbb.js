730592ef426d9d9f4806ac578e42e56a8f0eb1f7
Sam Edwards <nevitt1@umbc.edu>
2020-10-20 15:38:00 -0400

    Updates

---
+++ precache-manifest.da2726c1c5f62465fc276ea5b6669cbb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf3d8ba4d372499bf9db2069a727461",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "3ab56d1a9bf889de7501",
+    "url": "/react-portfolio/static/css/main.8e6592d2.chunk.css"
+  },
+  {
+    "revision": "46683e048878a2c6b7d4",
+    "url": "/react-portfolio/static/js/2.711f32ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.711f32ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ab56d1a9bf889de7501",
+    "url": "/react-portfolio/static/js/main.312aca23.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "c1d4cdb08dbd24b215f984eb56b0e338",
+    "url": "/react-portfolio/static/media/gerry.c1d4cdb0.png"
+  },
+  {
+    "revision": "9d29b37fc8778c9dd73f51570b1a7599",
+    "url": "/react-portfolio/static/media/github.9d29b37f.jpg"
+  },
+  {
+    "revision": "ec28fb7b94b2bd6f7ad5360e58aa2a31",
+    "url": "/react-portfolio/static/media/popcorn.ec28fb7b.png"
+  }
+]);
